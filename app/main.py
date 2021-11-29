62b89be14200366267f9ac3d5a12c7c5010a44f1
jibminJung <jibminjung@gmail.com>
2021-11-29 12:06:15 +0900

    Remove using global variable, Add hidden input to hold data

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def student():
 @app.route('/detail', methods = ['POST', 'GET'])
 def detail():
    if request.method == 'POST':
-      global detail
+      # global detail
       detail = dict()
       detail['Name'] = request.form.get('Name')
       detail['StudentNumber'] = request.form.get('StudentNumber')
@@ -21,14 +21,14 @@ def detail():
 def result():
    if request.method == 'POST':
       result = dict()
-      # result['Name'] = request.form.get('Name')
-      # result['StudentNumber'] = request.form.get('StudentNumber')
-      # result['Gender'] = request.form.get('Gender')
-      # result['Major'] = request.form.get('Major')
-      result['Name'] = detail['Name']
-      result['StudentNumber'] = detail['StudentNumber']
-      result['Gender'] = detail['Gender']
-      result['Major'] = detail['Major']
+      result['Name'] = request.form.get('Name')
+      result['StudentNumber'] = request.form.get('StudentNumber')
+      result['Gender'] = request.form.get('Gender')
+      result['Major'] = request.form.get('Major')
+      # result['Name'] = detail['Name']
+      # result['StudentNumber'] = detail['StudentNumber']
+      # result['Gender'] = detail['Gender']
+      # result['Major'] = detail['Major']
       college = request.form.get('College')
       if(college == '불교대학'): total = 4000000
       elif(college == '문과대학'): total = 4200000
@@ -36,9 +36,15 @@ def result():
       result['Tuition'] = format(int(total * (1 - int(request.form.get('Scholarship'))/100)), ',d')
       return render_template("result.html",result = result)
 
-@app.route('/calculate')
+@app.route('/calculate', methods = ['POST', 'GET'])
 def calculate():
-   return render_template('calculate.html')
+   if request.method == 'POST':
+      detail = dict()
+      detail['Name'] = request.form.get('Name')
+      detail['StudentNumber'] = request.form.get('StudentNumber')
+      detail['Gender'] = request.form.get('Gender')
+      detail['Major'] = request.form.get('Major')
+      return render_template('calculate.html',detail=detail)
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
